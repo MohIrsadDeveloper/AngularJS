45d0ab146b8c77a01e0c3b6515e64234770083a7
MohIrsadDeveloper <idrisimohdirshad@gmail.com>
2022-08-17 14:24:22 +0530

    Forms Validate

--- studentController.js
+++ studentController.js
@@ -1,17 +1,8 @@
 mainApp.controller('studentController', function ($scope) {
-    $scope.student = {
-        firstName : "Abdul",
-        lastName : "Idrisi",
-        fees : 500,
-        subjects : [
-            {name : 'Physics', marks : 70},
-            {name : 'Chemistry', marks : 80},
-            {name : 'Maths', marks : 65},
-        ],
-        fullName : function () {
-            var studentObject;
-            studentObject = $scope.student;
-            return studentObject.firstName + " "  + studentObject.lastName;
-        }
+    $scope.reset = function () {
+        $scope.firstName = "Abdul";
+        $scope.lastName = "Idrisi";
+        $scope.email = "abdul@gmail.com";
     }
+    $scope.reset();
 })
